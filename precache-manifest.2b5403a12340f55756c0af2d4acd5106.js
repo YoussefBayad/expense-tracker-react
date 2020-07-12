4e6fc29c6081e1cf27a83dd322d73814fc155bb8
Joseph bayad <jobayad66@gmail.com>
2020-07-12 17:28:27 +0100

    Updates

---
+++ precache-manifest.2b5403a12340f55756c0af2d4acd5106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60657ded5dddd2fc34b056378ff7a6e4",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "38eda7325222b48df940",
+    "url": "/expense-tracker-react/static/css/main.fb78d2d1.chunk.css"
+  },
+  {
+    "revision": "36c92918639ca1d535d8",
+    "url": "/expense-tracker-react/static/js/2.d015bc96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.d015bc96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38eda7325222b48df940",
+    "url": "/expense-tracker-react/static/js/main.7541f230.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
